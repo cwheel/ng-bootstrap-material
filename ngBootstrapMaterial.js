ae8c176376d457e0794d550e4bd373e357b69e40
nosedive25 <nosedive25@git.git>
2015-02-02 16:12:51 -0500

    Added forgoten semicolons

--- ngBootstrapMaterial.js
+++ ngBootstrapMaterial.js
@@ -3,25 +3,25 @@
 angular.module('ngBootstrapMaterial', []).directive('materialInput', function() {
     return {
         compile: function(elem, attr, tr) {
-          $.material.input("#" + attr.id)
+          $.material.input("#" + attr.id);
         }
     };
 }).directive('materialRipple', function() {
     return {
         compile: function(elem, attr, tr) {
-          $.material.ripples("#" + attr.id)
+          $.material.ripples("#" + attr.id);
         }
     };
 }).directive('materialCheckbox', function() {
     return {
         compile: function(elem, attr, tr) {
-          $.material.checkbox("#" + attr.id)
+          $.material.checkbox("#" + attr.id);
         }
     };
 }).directive('materialRadio', function() {
     return {
         compile: function(elem, attr, tr) {
-          $.material.radio("#" + attr.id)
+          $.material.radio("#" + attr.id);
         }
     };
 });
