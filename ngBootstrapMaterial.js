a1fd56e53ccbfb7f3c9ba3df88a6fb6cd8795577
nosedive25 <nosedive25@git.git>
2015-02-02 12:07:24 -0500

    Fixed variable name in compile estatement

--- ngBootstrapMaterial.js
+++ ngBootstrapMaterial.js
@@ -2,25 +2,25 @@
 
 angular.module('ngBootstrapMaterial', []).directive('materialInput', function() {
     return {
-        compile: function(scope, attr, tr) {
+        compile: function(elem, attr, tr) {
           $.material.input("#" + attr.id)
         }
     };
 }).directive('materialRipple', function() {
     return {
-        compile: function(scope, attr, tr) {
+        compile: function(elem, attr, tr) {
           $.material.ripples("#" + attr.id)
         }
     };
 }).directive('materialCheckbox', function() {
     return {
-        compile: function(scope, attr, tr) {
+        compile: function(elem, attr, tr) {
           $.material.checkbox("#" + attr.id)
         }
     };
 }).directive('materialRadio', function() {
     return {
-        compile: function(scope, attr, tr) {
+        compile: function(elem, attr, tr) {
           $.material.radio("#" + attr.id)
         }
     };
