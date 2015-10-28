7aa4399a552a4b03ef44a503b839327c63baf6e4
Cameron Wheeler <cameron@scelos.com>
2015-10-28 13:52:19 -0400

    Updated bower.json

--- ngBootstrapMaterial.js
+++ ngBootstrapMaterial.js
@@ -2,26 +2,38 @@
 
 angular.module('ngBootstrapMaterial', []).directive('materialInput', function() {
     return {
-        compile: function(elem, attr, tr) {
-          $.material.input(elem[0]);
+        compile: function(scope, attr, tr) {
+          $.material.input("#" + attr.id);
         }
     };
 }).directive('materialRipple', function() {
     return {
-        compile: function(elem, attr, tr) {
-          $.material.ripples(elem[0]));
+        compile: function(scope, attr, tr) {
+          $.material.ripples(scope[0]);
         }
     };
 }).directive('materialCheckbox', function() {
     return {
-        compile: function(elem, attr, tr) {
-          $.material.checkbox(elem[0]));
+        compile: function(scope, attr, tr) {
+          $.material.checkbox("#" + attr.id);
         }
     };
 }).directive('materialRadio', function() {
     return {
-        compile: function(elem, attr, tr) {
-          $.material.radio(elem[0]));
+        compile: function(scope, attr, tr) {
+          $.material.radio("#" + attr.id);
+        }
+    };
+}).directive('materialRippleNavigation', function() {
+    //Requires a data attribute to work, data-ripple (init w/ false)
+    return {
+        link: function(scope, element, attr) {
+            if (!attr['data-ripple']) {
+                if (attr.id.substring(0, "{{".length) !== "{{") {
+                    $.material.ripples(element[0]);
+                    attr.$set("data-ripple", true);
+                }
+            }
         }
     };
 });
